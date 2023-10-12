d560daae5fa1a76cd8314f6224339cdc412a27dc
mhdraslan <47928180+mhdraslan@users.noreply.github.com>
2023-10-12 14:30:00 +0400

    aa

--- setup.ps1
+++ setup.ps1
@@ -1,5 +1,5 @@
 $deploymentName = "SBC-Core-Deployment"
-$templateUri = "https://raw.githubusercontent.com/mhdraslan/ESLZ/main/SBC/SBC.json"
+$templateUri = "https://raw.githubusercontent.com/mhdraslan/SBC/main/SBC.json"
 $resourceGroupName = "rg-MS_Teams_DR-uaen-Prod-01"
 New-AzSubscriptionDeployment -Name $deploymentName `
                              -Location uaenorth `
